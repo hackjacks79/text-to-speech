835b0a914ab631fa091d6a3bb1e5a7b7083e0010
hackjacks79 <hackjacks79@gmail.com>
2022-05-12 10:55:50 +0530

    first commit

---
+++ src/main/java/com/wellsfargo/texttospeech/AppConfig.java
@@ -0,0 +1,18 @@
+package com.wellsfargo.texttospeech;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "app")
+@Component
+public class AppConfig {
+	private String credentialsPath;
+
+	  public String getCredentialsPath() {
+	    return this.credentialsPath;
+	  }
+
+	  public void setCredentialsPath(String credentialsPath) {
+	    this.credentialsPath = credentialsPath;
+	  }
+}
